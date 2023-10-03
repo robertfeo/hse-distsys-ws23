b63089bcd1616a31ad5c6d86ca1ccf6cb17e8066
Robert Fesko <fesko.robert@gmail.com>
2023-10-03 08:15:35 +0000

    backend

---
+++ backend/hsedistsys/src/main/java/hse/distsys/backend/HsedistsysApplication.java
@@ -0,0 +1,13 @@
+package hse.distsys.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsedistsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsedistsysApplication.class, args);
+	}
+
+}
---
+++ backend/hsedistsys/src/test/java/hse/distsys/hsedistsys/HsedistsysApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.distsys.hsedistsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsedistsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
