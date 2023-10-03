6f807825e6156e0535b4e1c988bba86479b1e609
Robert Fesko <fesko.robert@gmail.com>
2023-10-03 08:36:03 +0000

    init

---
+++ backend/notable/src/main/java/hse/distsys/notable/NotableApplication.java
@@ -0,0 +1,13 @@
+package hse.distsys.notable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotableApplication.class, args);
+	}
+
+}
---
+++ backend/notable/src/test/java/hse/distsys/notable/NotableApplicationTests.java
@@ -0,0 +1,13 @@
+package hse.distsys.notable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
