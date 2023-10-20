a641fc4da600dbb0a1d5443154c00526e8369b2e
Robert Fesko <fesko.robert@gmail.com>
2023-10-20 16:03:32 +0000

    update docker

--- backend/database/dummydata.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO todos (title, description) VALUES
-('Grocery Shopping', 'Buy vegetables, fruits, and dairy products.'),
-('Study for Exam', 'Review chapters 1 to 5 from the textbook.'),
-('Book Doctor Appointment', 'Schedule a check-up for next month.'),
-('Attend Team Meeting', 'Discuss the project milestones and updates.'),
-('Finish Assignment', 'Complete and submit the software engineering assignment before the deadline.');
