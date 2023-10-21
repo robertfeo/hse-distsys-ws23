619928ade0bd9f43f5a1b30931c0e855b5382036
Robert Fesko <fesko.robert@gmail.com>
2023-10-21 18:49:15 +0000

    update todo item

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -1,49 +1,83 @@
 import React from 'react';
-import { Typography, Box, Grid } from '@mui/material';
-import { Button } from "@material-tailwind/react";
+import DeleteIcon from './icons/DeleteIcon';
+import EditIcon from './icons/EditIcon';
+import CheckIcon from './icons/CheckIcon';
+import clsx from 'clsx';
+import { deleteTodoById, editTodoById } from '../api/todos';
 
-function TodoItem({ todo, onDelete, onEdit }) {
+const TodoItem = ({ searchTerm, item, onEditTodoItem, refreshTodos }) => {
+    const handleRemoveTodoItem = () => {
+        deleteTodoById(item.id)
+            .then(() => {
+                refreshTodos();
+            })
+            .catch((error) => {
+                console.error("There was an error deleting the todo", error);
+            });
+    };
+
+    const handleCheckTodoItem = () => {
+        editTodoById(item.id, !item.isChecked)
+            .then(() => {
+                refreshTodos();
+            })
+            .catch((error) => {
+                console.error("There was an error editing the todo", error);
+            });
+    };
+
     return (
-        <Box bgcolor="#e8eaf6" sx={{ minWidth: 500, padding: 2, borderBottom: '1px solid #e0e0e0', marginBottom: 1 }}>
-            <Grid container spacing={2} alignItems="center">
-                <Grid item xs={8}>
-                    <Typography sx={{ fontSize: 16 }}>
-                        {todo.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {todo.description ? todo.description : 'No description provided.'}
-                    </Typography>
-                </Grid>
-                <Grid item xs={4} container alignItems="center" justifyContent="flex-end">
-                    <Button variant='text' size='sm' className="rounded-full" onClick={() => onEdit(todo.id)}>
-                        Edit
-                    </Button>
-                    <Button variant='text' size='sm' className="rounded-full" color='red' onClick={() => onDelete(todo.id)}>
-                        Delete
-                    </Button>
-                    <Button variant='text' size='sm' color='red' className="rounded-full flex items-center gap-3">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={2}
-                            stroke="currentColor"
-                            className="h-5 w-5"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-                            />
-                        </svg>
-                    </Button>
-                </Grid>
-            </Grid>
-        </Box>
+        <div className={clsx(
+            'mt-2.5 flex w-full items-center justify-between bg-white p-4',
+            'rounded-lg border border-gray-200 shadow',
+        )}>
+            <span
+                className='font-normal text-gray-700'
+                dangerouslySetInnerHTML={{
+                    __html: searchTerm !== ''
+                        ? item.title.replace(searchTerm, `<span class="bg-blue-100 font-bold">${searchTerm}</span>`)
+                        : item.title,
+                }}
+            ></span>
+
+            <div className='flex gap-2'>
+
+                <button
+                    onClick={handleRemoveTodoItem}
+                    type='button'
+                    className={clsx(
+                        'flex h-10 w-10 items-center justify-center rounded-lg bg-rose-700',
+                        'hover:bg-rose-800 focus:outline-none focus:ring-4 focus:ring-rose-300',
+                    )}
+                >
+                    <DeleteIcon />
+                </button>
+
+                <button
+                    onClick={onEditTodoItem}
+                    type='button'
+                    className={clsx(
+                        'flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-700',
+                        'hover:bg-indigo-800 focus:outline-none focus:ring-4 focus:ring-indigo-300',
+                    )}
+                >
+                    <EditIcon />
+                </button>
+
+                <button
+                    onClick={handleCheckTodoItem}
+                    type='button'
+                    className={clsx(
+                        item.isChecked ? 'bg-emerald-700' : 'bg-gray-400',
+                        'flex h-10 w-10 items-center justify-center rounded-lg',
+                        'hover:bg-emerald-800 focus:outline-none focus:ring-4 focus:ring-emerald-300',
+                    )}
+                >
+                    <CheckIcon />
+                </button>
+            </div>
+        </div>
     );
-}
-
-
-
+};
 
 export default TodoItem;
