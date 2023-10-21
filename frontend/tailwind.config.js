d4dc4860b2f8d749131a4ec4a08bb7c1ada1dec5
Robert Fesko <fesko.robert@gmail.com>
2023-10-21 17:08:33 +0000

    update colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,26 @@ module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'emerald': {
+        50: '#ecfdf5',
+        100: '#d1fae5',
+        200: '#a7f3d0',
+        300: '#6ee7b7',
+        400: '#34d399',
+        500: '#10b981',
+        600: '#059669',
+        700: '#047857',
+        800: '#065f46',
+        900: '#064e3b',
+      }
+    },
   },
   plugins: [],
 });
