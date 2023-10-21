e1979c9338488040a533f5fea1b4366c8b1a4336
Robert Fesko <fesko.robert@gmail.com>
2023-10-21 16:13:24 +0000

    added heading

--- frontend/src/components/Heading.js
+++ frontend/src/components/Heading.js
@@ -2,8 +2,8 @@
 const Heading = () => {
     return (
         <h1 className='py-3 text-center text-4xl font-bold text-gray-900'>
-            <span className='bg-gradient-to-r from-blue-400 to-emerald-600 bg-clip-text text-transparent'>
-                Smatyx
+            <span className='bg-gradient-to-r from-blue-400 to-green-800 bg-clip-text text-transparent'>
+                Robert's
             </span>
             &nbsp;
             <span>Todos App</span>
