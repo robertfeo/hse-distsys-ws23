20c2ca1ee799454e8b2e90ba8066e791fd3285db
Robert B. Fesko <fesko.robert@gmail.com>
2023-10-23 14:49:09 +0000

    update backend

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+INSERT INTO todoitem (title, isChecked) VALUES
+('Grocery Shopping', true);
+
+INSERT INTO todoitem (title, isChecked) VALUES
+('Study for Exam', false);
+
+INSERT INTO todoitem (title, isChecked) VALUES
+('Book Doctor Appointment', false);
+
+INSERT INTO todoitem (title, isChecked) VALUES
+('Attend Team Meeting', false);
+
+INSERT INTO todoitem (title, isChecked) VALUES
+('Finish Todo App', true);
