22d4cdee2481fc8597ba84b3276943cec930fa0c
Robert B. Fesko <fesko.robert@gmail.com>
2023-10-23 15:16:02 +0000

    update import sql

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,14 +1,5 @@
-INSERT INTO todoitem (title, isChecked) VALUES
-('Grocery Shopping', true);
-
-INSERT INTO todoitem (title, isChecked) VALUES
-('Study for Exam', false);
-
-INSERT INTO todoitem (title, isChecked) VALUES
-('Book Doctor Appointment', false);
-
-INSERT INTO todoitem (title, isChecked) VALUES
-('Attend Team Meeting', false);
-
-INSERT INTO todoitem (title, isChecked) VALUES
-('Finish Todo App', true);
+INSERT INTO todo_item (title, isChecked) VALUES ('Grocery Shopping', true);
+INSERT INTO todo_item (title, isChecked) VALUES ('Study for Exam', false);
+INSERT INTO todo_item (title, isChecked) VALUES ('Book Doctor Appointment', false);
+INSERT INTO todo_item (title, isChecked) VALUES ('Attend Team Meeting', false);
+INSERT INTO todo_item (title, isChecked) VALUES ('Finish Todo App', true);
