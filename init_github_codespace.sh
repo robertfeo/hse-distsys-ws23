08d430ce0efbf7e5570aa19a945c8b19dd73c141
Robert Fesko <fesko.robert@gmail.com>
2023-12-04 08:05:49 +0000

    update docker

--- init_github_codespace.sh
+++ init_github_codespace.sh
@@ -1,13 +1,14 @@
 sudo apt update -q
 sudo apt upgrade -y -q
-sdk install java 17.0.4.1-tem < /dev/null
-NODEJS_VERSION="21.0.0"
-nvm install $NODEJS_VERSION -y
-nvm use $NODEJS_VERSION && nvm alias default $NODEJS_VERSION
+#sdk install java 17.0.4.1-tem < /dev/null
+#NODEJS_VERSION="21.0.0"
+#nvm install $NODEJS_VERSION -y
+#nvm use $NODEJS_VERSION && nvm alias default $NODEJS_VERSION
+echo Y | sdk upgrade
+npm install -g npm@10.2.1
 cd backend/
 mvn clean package
 cd ..
-npm install -g npm@10.2.1
 cd frontend/
 npm install --silent
 cd ..
