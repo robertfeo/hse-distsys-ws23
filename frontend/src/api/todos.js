4b68b406feb49261f9e50882fd9bb617ee6e32db
Robert B. Fesko <fesko.robert@gmail.com>
2023-12-07 22:52:13 +0000

    test heroku deploy

--- frontend/src/api/todos.js
+++ frontend/src/api/todos.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const herokuApiUrl = process.env.REACT_APP_BACKEND_URL ? `http://backend:5000/api` : 'https://robertf-todoapp-api-175ccf43153c.herokuapp.com/api';
+const herokuApiUrl = process.env.REACT_APP_BACKEND_URL ? `http://backend:8080/api` : 'https://robertf-todoapp-api-175ccf43153c.herokuapp.com/api';
 const apiUrl = herokuApiUrl || 'http://localhost:8080/api';
 
 export const fetchTodos = () => {
